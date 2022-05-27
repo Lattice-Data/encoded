3631e56adee94a57d8a884086b03a9d7df1ea72b
jason Hilton <jahilton@stanford.edu>
2022-05-27 11:15:41 -0700

    remove lab from User schema

--- src/encoded/tests/test_upgrade_user.py
+++ src/encoded/tests/test_upgrade_user.py
@@ -13,3 +13,10 @@ def test_user_upgrade_2_3(upgrader, user_base):
 	value = upgrader.upgrade('user', user_base, current_version='2', target_version='3')
 	assert value['job_title'] == 'principal investigator'
 	assert value['schema_version'] == '3'
+
+
+def test_user_upgrade_3_4(upgrader, user_base, lab_base):
+	user_base['lab'] = lab_base['uuid']
+	value = upgrader.upgrade('user', user_base, current_version='3', target_version='4')
+	assert 'lab' not in value
+	assert value['schema_version'] == '4'

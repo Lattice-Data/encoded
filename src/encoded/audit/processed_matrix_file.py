7440bec464ede3dcaa60d6f62cd1bc148347d5c8
jason Hilton <jahilton@stanford.edu>
2022-09-16 12:43:31 -0700

    check for duplicate input raw data

--- src/encoded/audit/processed_matrix_file.py
+++ src/encoded/audit/processed_matrix_file.py
@@ -27,13 +27,35 @@ def ontology_check_dis(value, system):
             yield AuditFailure('incorrect ontology term', detail, 'ERROR')
 
 
+def duplicated_derfrom(value, system):
+    all_seq_files = {}
+    for raw in value['derived_from']:
+        for seqfile in raw['derived_from']:
+            if seqfile in all_seq_files:
+                all_seq_files[seqfile].append(raw['accession'])
+            else:
+                all_seq_files[seqfile] = [raw['accession']]
+
+    for k,v in all_seq_files.items():
+        if len(v) > 1:
+            detail = ('File {} has duplicated input {} from RawMatrixFiles {}.'.format(
+                audit_link(value['accession'], value['@id']),
+                k,
+                ','.join(v)
+                )
+            )
+            yield AuditFailure('duplicate input raw data', detail, 'ERROR')
+
+
 function_dispatcher = {
-    'ontology_check_dis': ontology_check_dis
+    'ontology_check_dis': ontology_check_dis,
+    'duplicated_derfrom': duplicated_derfrom
 }
 
 @audit_checker('ProcessedMatrixFile',
                frame=[
-                'experimental_variable_disease'
+                'experimental_variable_disease',
+                'derived_from'
                 ])
 def audit_processed_matrix_file(value, system):
     for function_name in function_dispatcher.keys():

90de81e6db8fba20304f1eb37833fe914f384ef8
jason Hilton <jahilton@stanford.edu>
2022-09-19 12:55:11 -0700

    add checks for ProMxFile mappings

--- src/encoded/audit/processed_matrix_file.py
+++ src/encoded/audit/processed_matrix_file.py
@@ -8,6 +8,80 @@ from .formatter import (
 )
 
 
+def mappings_antibodies(value,system):
+    if value.get('antibody_mappings'):
+        labels = [am['label'] for am in value['antibody_mappings']]
+        dups = [l for l in labels if labels.count(l) > 1]
+        if dups:
+            detail = ('File {} contains duplicate labels {} in antibody_mappings.'.format(
+                audit_link(value['accession'], value['@id']),
+                ','.join(set(dups))
+                )
+            )
+            yield AuditFailure('antibody_mapping error', detail, 'ERROR')
+
+        antibodies = []
+        for l in value['libraries']:
+            for susp in l['derived_from']:
+                antibodies.extend(susp.get('feature_antibodies',[]))
+        for am in value['antibody_mappings']:
+            if am['antibody'] not in antibodies:
+                detail = ('File {} contains {} in antibody_mappings but is not linked to this Antibody.'.format(
+                    audit_link(value['accession'], value['@id']),
+                    am['antibody']
+                    )
+                )
+                yield AuditFailure('antibody_mapping error', detail, 'ERROR')
+
+
+def mappings_donors(value,system):
+    if value.get('donor_mappings'):
+        labels = [dm['label'] for dm in value['donor_mappings']]
+        dups = [l for l in labels if labels.count(l) > 1]
+        if dups:
+            detail = ('File {} contains duplicate labels {} in donor_mappings.'.format(
+                audit_link(value['accession'], value['@id']),
+                ','.join(set(dups))
+                )
+            )
+            yield AuditFailure('donor_mappings error', detail, 'ERROR')
+
+        donors = []
+        for l in value['libraries']:
+            donors.extend(l['donors'])
+        for dm in value['donor_mappings']:
+            if dm['donor'] not in donors:
+                detail = ('File {} contains {} in donor_mappings but is not linked to this Donor.'.format(
+                    audit_link(value['accession'], value['@id']),
+                    dm['donor']
+                    )
+                )
+                yield AuditFailure('donor_mappings error', detail, 'ERROR')
+
+
+def mappings_matrices(value,system):
+    if value.get('cell_label_mappings'):
+        labels = [clm['label'] for clm in value['cell_label_mappings']]
+        dups = [l for l in labels if labels.count(l) > 1]
+        if dups:
+            detail = ('File {} contains duplicate labels {} in cell_label_mappings.'.format(
+                audit_link(value['accession'], value['@id']),
+                ','.join(set(dups))
+                )
+            )
+            yield AuditFailure('cell_label_mappings error', detail, 'ERROR')
+
+        for clm in value['cell_label_mappings']:
+            derived_from = [d['@id'] for d in value['derived_from']]
+            if clm['raw_matrix'] not in derived_from:
+                detail = ('File {} contains {} in cell_label_mappings but not derived_from.'.format(
+                    audit_link(value['accession'], value['@id']),
+                    clm['raw_matrix']
+                    )
+                )
+                yield AuditFailure('cell_label_mappings error', detail, 'ERROR')
+
+
 def ontology_check_dis(value, system):
     field = 'experimental_variable_disease'
     dbs = ['MONDO']
@@ -49,13 +123,18 @@ def duplicated_derfrom(value, system):
 
 function_dispatcher = {
     'ontology_check_dis': ontology_check_dis,
-    'duplicated_derfrom': duplicated_derfrom
+    'duplicated_derfrom': duplicated_derfrom,
+    'mappings_antibodies': mappings_antibodies,
+    'mappings_donors': mappings_donors,
+    'mappings_matrices': mappings_matrices
 }
 
 @audit_checker('ProcessedMatrixFile',
                frame=[
                 'experimental_variable_disease',
-                'derived_from'
+                'derived_from',
+                'libraries',
+                'libraries.derived_from'
                 ])
 def audit_processed_matrix_file(value, system):
     for function_name in function_dispatcher.keys():

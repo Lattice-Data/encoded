a0cfa67e65ad01727dc90bf17ffe966a371d9596
jason Hilton <jahilton@stanford.edu>
2022-10-02 16:10:46 -0700

    turn ethnicity to a list

--- src/encoded/tests/fixtures/schemas/human_postnatal_donor.py
+++ src/encoded/tests/fixtures/schemas/human_postnatal_donor.py
@@ -7,7 +7,7 @@ def human_postnatal_donor_base(testapp, adult_ontology, european_ontology):
         'sex': 'male',
         'development_ontology': adult_ontology['@id'],
         'age': 'unknown',
-        'ethnicity': european_ontology['@id'],
+        'ethnicity': [european_ontology['@id']],
         'donor_id': 'USCS33938'
     }
     return testapp.post_json('/human_postnatal_donor', item, status=201).json['@graph'][0]

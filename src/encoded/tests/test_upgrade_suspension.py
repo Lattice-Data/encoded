3e49c4f4f91ae814aa060734acd8267de8a15146
jason Hilton <jahilton@stanford.edu>
2022-10-03 08:13:49 -0700

    add susp upgrade to 5

--- src/encoded/tests/test_upgrade_suspension.py
+++ src/encoded/tests/test_upgrade_suspension.py
@@ -7,6 +7,7 @@ def test_suspension_upgrade_1_2(upgrader, suspension_base):
 	assert 'biosample_ontology' not in value
 	assert value['schema_version'] == '2'
 
+
 def test_suspension_upgrade_2_3(upgrader, suspension_base):
 	suspension_base['url'] = 'https://www.protocols.io/'
 	value = upgrader.upgrade('suspension', suspension_base, current_version='2', target_version='3')
@@ -14,8 +15,19 @@ def test_suspension_upgrade_2_3(upgrader, suspension_base):
 	assert value['urls'] == ['https://www.protocols.io/']
 	assert value['schema_version'] == '3'
 
+
+'''
 def test_suspension_upgrade_3_4(upgrader, suspension_base):
 	suspension_base['dissociation_time'] = 10
 	value = upgrader.upgrade('suspension', suspension_base, current_version='3', target_version='4')
 	assert value['dissociation_time'] == '10'
 	assert value['schema_version'] == '4'
+'''
+
+
+def test_suspension_upgrade_4_5(upgrader, suspension_base):
+	suspension_base['red_blood_cell_lysis'] = True
+	value = upgrader.upgrade('suspension', suspension_base, current_version='4', target_version='5')
+	assert value['red_blood_cell_removal'] == True
+	assert 'red_blood_cell_lysis' not in value
+	assert value['schema_version'] == '5'

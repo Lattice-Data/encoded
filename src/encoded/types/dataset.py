8e9987eb3be8fd392d50900d82158b4d2d110cf0
jason Hilton <jahilton@stanford.edu>
2022-10-17 11:30:11 -0700

    add 4 calculated props to DS

--- src/encoded/types/dataset.py
+++ src/encoded/types/dataset.py
@@ -104,6 +104,66 @@ class Dataset(Item):
 
 
     @calculated_property(schema={
+        "title": "Donor Count",
+        "description": "The number of unique Donors that belong to this Dataset.",
+        "comment": "Do not submit. This is a calculated property",
+        "type": "integer",
+        "notSubmittable": True,
+    })
+    def donor_count(self, request, libraries=None):
+        if libraries:
+            libraries = paths_filtered_by_status(request, libraries)
+            donors = []
+            for l in libraries:
+                donors.extend(request.embed(l, '@@object').get('donors'))
+            return(len(set(donors)))
+
+
+    @calculated_property(schema={
+        "title": "Library Count",
+        "description": "The number of Libraries that belong to this Dataset.",
+        "comment": "Do not submit. This is a calculated property",
+        "type": "integer",
+        "notSubmittable": True,
+    })
+    def library_count(self, request, libraries=None):
+        if libraries:
+            return len(paths_filtered_by_status(request, libraries))
+
+
+    @calculated_property(schema={
+        "title": "Read Count",
+        "description": "The number of reads from the Libraries that belong to this Dataset.",
+        "comment": "Do not submit. This is a calculated property",
+        "type": "integer",
+        "notSubmittable": True,
+    })
+    def read_count(self, request, libraries=None):
+        if libraries:
+            libraries = paths_filtered_by_status(request, libraries)
+            reads = 0
+            for l in libraries:
+                reads += request.embed(l, '@@object').get('read_count',0)
+            return reads
+
+
+    @calculated_property(schema={
+        "title": "Observation Count",
+        "description": "The number of observations from the Libraries that belong to this Dataset.",
+        "comment": "Do not submit. This is a calculated property",
+        "type": "integer",
+        "notSubmittable": True,
+    })
+    def observation_count(self, request, libraries=None):
+        if libraries:
+            libraries = paths_filtered_by_status(request, libraries)
+            obs = 0
+            for l in libraries:
+                obs += request.embed(l, '@@object').get('observation_count',0)
+            return obs
+
+
+    @calculated_property(schema={
         "title": "Original files",
         "description": "The DataFiles that belong to this Dataset, regardless of status.",
         "comment": "Do not submit. This is a calculated property",

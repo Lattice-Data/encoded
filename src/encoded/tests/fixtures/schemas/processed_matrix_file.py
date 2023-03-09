f98368b2961ff8e419f24f9d9055a52eee309f51
jason Hilton <jahilton@stanford.edu>
2023-03-09 13:05:18 -0800

    require ProcMxFile.description

--- src/encoded/tests/fixtures/schemas/processed_matrix_file.py
+++ src/encoded/tests/fixtures/schemas/processed_matrix_file.py
@@ -10,6 +10,7 @@ def processed_matrix_file_base(testapp, dataset_base, raw_matrix_file_base):
         'output_types': ['gene quantifications'],
         'derived_from': [raw_matrix_file_base['uuid']],
         'is_primary_data': 'True',
-        'X_normalized': False
+        'X_normalized': False,
+        'description': 'some final mx'
     }
     return testapp.post_json('/processed_matrix_file', item).json['@graph'][0]

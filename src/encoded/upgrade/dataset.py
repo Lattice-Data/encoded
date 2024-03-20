ae2c87834f4d8196f7cf653eef785214b5a0a1ba
jahilton <jahilton@stanford.edu>
2024-03-19 17:15:53 -0700

    upgrade hca portal links

--- src/encoded/upgrade/dataset.py
+++ src/encoded/upgrade/dataset.py
@@ -3,14 +3,6 @@ from snovault import upgrade_step
 
 @upgrade_step('dataset', '1', '2')
 def dataset_1_2(value, system):
-	if 'corresponding_contributor' in value:
-		value['corresponding_contributors'] = [value['corresponding_contributor']]
-		del value['corresponding_contributor']
-
-
-#never updated default version to 2
-@upgrade_step('dataset', '1', '3')
-def dataset_1_3(value, system):
 	if 'urls' in value:
 		new_urls = []
 		for url in value['urls']:

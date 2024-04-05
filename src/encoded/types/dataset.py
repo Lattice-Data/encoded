dd54379f1d2e44cd77f71ff9d8ce1678a85c64d5
jahilton <jahilton@stanford.edu>
2024-04-04 20:17:14 -0700

    remove extra files lists

--- src/encoded/types/dataset.py
+++ src/encoded/types/dataset.py
@@ -159,68 +159,6 @@ class Dataset(Item):
             return paths_filtered_by_status(request, original_files)
 
 
-    @calculated_property(schema={
-        "title": "Contributing files",
-        "description": "The DataFiles that contribute to this Dataset's data products but do not belong to this Dataset, typically reference files.",
-        "comment": "Do not submit. This is a calculated property",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "DataFile",
-        },
-    })
-    def contributing_files(self, request, status, original_files=None):
-        if original_files:
-            derived_from = set()
-            for f in original_files:
-                f_obj = request.embed(f, '@@object?skip_calculated=true')
-                f_df = f_obj.get('derived_from')
-                if isinstance(f_df, str):
-                    derived_from.add(f_df)
-                else:
-                    derived_from.update(f_df)
-
-            outside_ids = list(derived_from.difference(original_files))
-            outside_files = []
-            for i in outside_ids:
-                outsideObject = request.embed(i, '@@object')
-                if 'File' in outsideObject.get('@type'):
-                    outside_files.append(i)
-
-            if status in ('released'):
-                contributing = paths_filtered_by_status(
-                    request, outside_files,
-                    include=('released',),
-                )
-            else:
-                contributing = paths_filtered_by_status(
-                    request, outside_files,
-                    exclude=('revoked', 'deleted', 'replaced'),
-                )
-            if contributing:
-                return contributing
-
-
-    @calculated_property(schema={
-        "title": "Revoked files",
-        "description": "The DataFiles that are revoked and belong to this Dataset.",
-        "comment": "Do not submit. This is a calculated property",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "DataFile",
-        },
-    })
-    def revoked_files(self, request, original_files=None):
-        if original_files:
-            revoked = [
-                path for path in original_files
-                if item_is_revoked(request, path)
-            ]
-            if revoked:
-                return revoked
-
-
     @calculated_property(define=True, schema={
         "title": "CELLxGENE URLs",
         "description": "Links to the representation of this dataset in CZ CELLxGENE Discover.",

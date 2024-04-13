f56118dbbd10875f143c5457060b7e7330c1e5c6
jahilton <jahilton@stanford.edu>
2024-04-13 09:19:10 -0700

    remove tissues_sampled from cellannotations

--- src/encoded/types/cell_annotation.py
+++ src/encoded/types/cell_annotation.py
@@ -18,31 +18,6 @@ class CellAnnotation(Item):
     item_type = 'cell_annotation'
     schema = load_schema('encoded:schemas/cell_annotation.json')
     embedded = [
-        'cell_ontology',
-        'tissues_sampled'
+        'cell_ontology'
     ]
     audit_inherit = ['cell_ontology']
-
-
-    @calculated_property(schema={
-        "title": "Tissues sampled",
-        "description": "",
-        "comment": "Do not submit. This is a calculated property",
-        "type": "array",
-        "items": {
-            "type": "string",
-            "linkTo": "OntologyTerm"
-        },
-        "notSubmittable": True,
-    })
-    def tissues_sampled(self, request, matrix_files=None):
-        if matrix_files:
-            libs = set()
-            for mf in matrix_files:
-                mf_obj = request.embed(mf, '@@object')
-                libs.update(mf_obj.get('libraries'))
-            onts = set()
-            for l in libs:
-                lib_obj = request.embed(l, '@@object')
-                onts.update(lib_obj.get('biosample_ontologies'))
-            return list(onts)

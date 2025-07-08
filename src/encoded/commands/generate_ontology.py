b06185419a357a155fe3c57be5c4715532db95d7
jahilton <jahilton@stanford.edu>
2025-07-07 21:12:28 -0700

    update ontologies

--- src/encoded/commands/generate_ontology.py
+++ src/encoded/commands/generate_ontology.py
@@ -305,12 +305,12 @@ def getTermStructure():
 def main():
     ''' Downloads various ontologies and create a JSON file '''
 
-    cl_url = 'https://github.com/obophenotype/cell-ontology/releases/download/v2025-02-13/cl.owl'
-    efo_url = 'https://github.com/EBISPOT/efo/releases/download/v3.75.0/efo.owl'
+    cl_url = 'https://github.com/obophenotype/cell-ontology/releases/download/v2025-04-10/cl.owl'
+    efo_url = 'https://github.com/EBISPOT/efo/releases/download/v3.78.0/efo.owl'
     hancestro_url = 'https://raw.githubusercontent.com/EBISPOT/hancestro/3.0/hancestro-base.owl'
     hsapdv_url = 'https://github.com/obophenotype/developmental-stage-ontologies/releases/download/v2025-01-23/hsapdv.owl'
-    mondo_url = 'https://github.com/monarch-initiative/mondo/releases/download/v2025-02-04/mondo.owl'
-    uberon_url = 'https://github.com/obophenotype/uberon/releases/download/v2025-01-15/uberon.owl'
+    mondo_url = 'https://github.com/monarch-initiative/mondo/releases/download/v2025-05-06/mondo.owl'
+    uberon_url = 'https://github.com/obophenotype/uberon/releases/download/v2025-05-28/uberon.owl'
     ncit_url = 'http://purl.obolibrary.org/obo/ncit/releases/2024-05-07/ncit.owl'
 
     url_whitelist = {
